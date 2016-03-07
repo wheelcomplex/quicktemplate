4a1df8b7b1e3a02583e43a4dabc078978f09fc95
Aliaksandr Valialkin <valyala@gmail.com>
2016-03-07 18:41:38 +0200

    parser 'test'

---
+++ parser_test.go
@@ -0,0 +1,35 @@
+package quicktemplate
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	s := `
+this is a sample template
+{% code
+import (
+	"foo"
+	"bar"
+)
+%}
+
+this is a sample func
+{% func foobar(s string) %}
+	{%comment%}this %}{% is a comment{%endcomment%}
+	he` + "`" + `llo, {%s s %}
+	{% code panic("foobar") %} aaa
+	{% plain %}aaa {% ` + "`" + `foo %} bar{% endplain %}
+bbb
+{% endfunc %}
+
+this is a tail
+`
+
+	r := bytes.NewBufferString(s)
+	w := &bytes.Buffer{}
+	Parse(w, r)
+
+	t.Fatalf("result\n%s\n", w.Bytes())
+}
