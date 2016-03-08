54163b24b0ccc3d57b3e03727e210e5b3d9f1c99
Aliaksandr Valialkin <valyala@gmail.com>
2016-03-08 22:00:36 +0200

    added missing util.go

---
+++ util.go
@@ -0,0 +1,23 @@
+package quicktemplate
+
+import (
+	"unicode"
+)
+
+func stripLeadingSpace(b []byte) []byte {
+	for len(b) > 0 && isSpace(b[0]) {
+		b = b[1:]
+	}
+	return b
+}
+
+func stripTrailingSpace(b []byte) []byte {
+	for len(b) > 0 && isSpace(b[len(b)-1]) {
+		b = b[:len(b)-1]
+	}
+	return b
+}
+
+func isSpace(c byte) bool {
+	return unicode.IsSpace(rune(c))
+}
