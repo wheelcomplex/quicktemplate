08c82d5d2dbc247a86b32212aece01348134ed2f
Aliaksandr Valialkin <valyala@gmail.com>
2016-03-11 23:49:25 +0200

    prefix qtc log messages with 'qtc: '

--- qtc/main.go
+++ qtc/main.go
@@ -18,13 +18,15 @@ var (
 	ext = flag.String("ext", "qtpl", "Only files with this extension are compiled")
 )
 
+var logger = log.New(os.Stderr, "qtc: ", log.LstdFlags)
+
 var filesCompiled int
 
 func main() {
 	flag.Parse()
 
 	if len(*ext) == 0 {
-		log.Fatalf("ext cannot be empty")
+		logger.Fatalf("ext cannot be empty")
 	}
 	if len(*dir) == 0 {
 		*dir = "."
@@ -33,27 +35,28 @@ func main() {
 		*ext = "." + *ext
 	}
 
+	logger.Printf("Compiling *%s template files in directory %q", *ext, *dir)
 	compileDir(*dir)
-	log.Printf("Total files compiled: %d", filesCompiled)
+	logger.Printf("Total files compiled: %d", filesCompiled)
 }
 
 func compileDir(path string) {
 	fi, err := os.Stat(path)
 	if err != nil {
-		log.Fatalf("cannot compile files in %q: %s", path, err)
+		logger.Fatalf("cannot compile files in %q: %s", path, err)
 	}
 	if !fi.IsDir() {
-		log.Fatalf("cannot compile files in %q: it is not directory", path)
+		logger.Fatalf("cannot compile files in %q: it is not directory", path)
 	}
 	d, err := os.Open(path)
 	if err != nil {
-		log.Fatalf("cannot compile files in %q: %s", path, err)
+		logger.Fatalf("cannot compile files in %q: %s", path, err)
 	}
 	defer d.Close()
 
 	fis, err := d.Readdir(-1)
 	if err != nil {
-		log.Fatalf("cannot read files in %q: %s", path, err)
+		logger.Fatalf("cannot read files in %q: %s", path, err)
 	}
 
 	var names []string
@@ -81,25 +84,25 @@ func compileDir(path string) {
 
 func compileFile(infile string) {
 	outfile := infile + ".go"
-	log.Printf("Compiling %q to %q...", infile, outfile)
+	logger.Printf("Compiling %q to %q...", infile, outfile)
 	inf, err := os.Open(infile)
 	if err != nil {
-		log.Fatalf("cannot open file %q: %s", infile, err)
+		logger.Fatalf("cannot open file %q: %s", infile, err)
 	}
 	defer inf.Close()
 
 	outf, err := os.Create(outfile)
 	if err != nil {
-		log.Fatalf("cannot create file %q: %s", outfile, err)
+		logger.Fatalf("cannot create file %q: %s", outfile, err)
 	}
 	defer outf.Close()
 
 	packageName, err := getPackageName(infile)
 	if err != nil {
-		log.Fatalf("cannot determine package name for %q: %s", infile, err)
+		logger.Fatalf("cannot determine package name for %q: %s", infile, err)
 	}
 	if err = parse(outf, inf, infile, packageName); err != nil {
-		log.Fatalf("error when parsing file %q: %s", infile, err)
+		logger.Fatalf("error when parsing file %q: %s", infile, err)
 	}
 	filesCompiled++
 }
