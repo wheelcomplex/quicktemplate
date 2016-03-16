6aef9d6051e9712c4b451ac508a013cc6ca8eeb5
Aliaksandr Valialkin <valyala@gmail.com>
2016-03-16 09:46:20 +0200

    added a test for writer

---
+++ writer_test.go
@@ -0,0 +1,42 @@
+package quicktemplate
+
+import (
+	"testing"
+)
+
+func TestWriter(t *testing.T) {
+	bb := AcquireByteBuffer()
+	qw := AcquireWriter(bb)
+	w := qw.W()
+	bbNew, ok := w.(*ByteBuffer)
+	if !ok {
+		t.Fatalf("W() must return ByteBuffer, not %T", w)
+	}
+	if bbNew != bb {
+		t.Fatalf("unexpected ByteBuffer returned: %p. Expecting %p", bbNew, bb)
+	}
+
+	wn := qw.N()
+	we := qw.E()
+
+	wn.S("<a></a>")
+	wn.D(123)
+	wn.Z([]byte("'"))
+	wn.Q("foo")
+	wn.F(1.23)
+
+	we.S("<a></a>")
+	we.D(321)
+	we.Z([]byte("'"))
+	we.Q("foo")
+	we.F(1.23)
+
+	ReleaseWriter(qw)
+
+	expectedS := `<a></a>123'"foo"1.23&lt;a&gt;&lt;/a&gt;321&#39;&quot;foo&quot;1.23`
+	if string(bb.B) != expectedS {
+		t.Fatalf("unexpected output: %q. Expecting %q", bb.B, expectedS)
+	}
+
+	ReleaseByteBuffer(bb)
+}
