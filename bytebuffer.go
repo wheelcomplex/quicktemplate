2a56560d0070f1b67193d7681271e420278efac8
Aliaksandr Valialkin <valyala@gmail.com>
2016-06-22 20:37:10 +0300

    Use github.com/valyala/bytebufferpool, since it should protect from memory fragmentation related to differently-sized buffers in concurrent use

--- bytebuffer.go
+++ bytebuffer.go
@@ -1,28 +1,23 @@
 package quicktemplate
 
 import (
-	"sync"
+	"github.com/valyala/bytebufferpool"
 )
 
 // ByteBuffer implements io.Writer on top of byte slice.
 //
 // Recycle byte buffers via AcquireByteBuffer and ReleaseByteBuffer
 // in order to reduce memory allocations.
-type ByteBuffer struct {
-	// B is a byte slice backing byte buffer.
-	// All the data written via Write is appended here.
-	B []byte
-}
+type ByteBuffer bytebufferpool.ByteBuffer
 
 // Write implements io.Writer.
-func (bb *ByteBuffer) Write(p []byte) (int, error) {
-	bb.B = append(bb.B, p...)
-	return len(p), nil
+func (b *ByteBuffer) Write(p []byte) (int, error) {
+	return bb(b).Write(p)
 }
 
 // Reset resets the byte buffer.
-func (bb *ByteBuffer) Reset() {
-	bb.B = bb.B[:0]
+func (b *ByteBuffer) Reset() {
+	bb(b).Reset()
 }
 
 // AcquireByteBuffer returns new ByteBuffer from the pool.
@@ -30,20 +25,17 @@ func (bb *ByteBuffer) Reset() {
 // Return unneeded buffers to the pool by calling ReleaseByteBuffer
 // in order to reduce memory allocations.
 func AcquireByteBuffer() *ByteBuffer {
-	v := byteBufferPool.Get()
-	if v == nil {
-		return &ByteBuffer{}
-	}
-	return v.(*ByteBuffer)
+	return (*ByteBuffer)(bytebufferpool.AcquireByteBuffer())
 }
 
 // ReleaseByteBuffer retruns byte buffer to the pool.
 //
 // Do not access byte buffer after returning it to the pool,
 // otherwise data races may occur.
-func ReleaseByteBuffer(bb *ByteBuffer) {
-	bb.Reset()
-	byteBufferPool.Put(bb)
+func ReleaseByteBuffer(b *ByteBuffer) {
+	bytebufferpool.ReleaseByteBuffer(bb(b))
 }
 
-var byteBufferPool sync.Pool
+func bb(b *ByteBuffer) *bytebufferpool.ByteBuffer {
+	return (*bytebufferpool.ByteBuffer)(b)
+}
